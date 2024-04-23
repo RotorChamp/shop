2e2357f3cd8d3d34ed9ec60cdf3278191dbd18cc
RotorChamp <152025503+RotorChamp@users.noreply.github.com>
2024-04-23 21:59:11 +0300

    Header Update

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <span className='logo'>Hype Shoes</span>
+            <ul className='nav'>
+              <li>Про нас</li>
+              <li>Контакты</li>
+              <li>Кабинет</li>
+            </ul>
+        </div>
+        <div className='presentation'></div>
+    </header>
+  )
+}
+    
